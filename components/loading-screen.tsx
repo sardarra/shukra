cc6575bc0d6cd409a6fa77fb7140451f07dc082a
sardarra <92699150+sardarra@users.noreply.github.com>
2026-06-24 15:00:04 -0400

    Update loading-screen.tsx

--- components/loading-screen.tsx
+++ components/loading-screen.tsx
@@ -3,9 +3,11 @@ import { Spinner } from "./ui/spinner";
 export function LoadingScreen() {
 
     return (
-        <div className="flex items-center justify-center h-screen">
-            <Spinner className="size-10 animate-spin" />
-            <p className="text-sm text-muted-foreground">Loading...</p>
+        <div className="flex flex-col items-center justify-center h-screen gap-4">
+            <Spinner className="size-10 animate-spin" style={{color: "#7C6103"}} />
+
+            <p className="text-sm text-muted-foreground">Powering up Shukra</p>
     </div>
   );
 }
+
